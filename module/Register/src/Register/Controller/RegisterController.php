88566da4e79da21cdb5b3bcbf5434399e5e7b26b
Paul Ivan'ko <ipaul.v@gmail.com>
2014-04-18 12:47:33 +0300

    create form register

--- module/Register/src/Register/Controller/RegisterController.php
+++ module/Register/src/Register/Controller/RegisterController.php
@@ -4,6 +4,8 @@ namespace Register\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\Form\Annotation\AnnotationBuilder;
+use Register\Model\RegisterNote;
 
 class RegisterController extends AbstractActionController
 {
@@ -11,6 +13,24 @@ class RegisterController extends AbstractActionController
     public function indexAction()
     {
         return new ViewModel();
+    }
+
+    public function addAction()
+    {
+        $registerNote = new RegisterNote();
+        $builder = new AnnotationBuilder();
+        $form = $builder->createForm($registerNote);
+
+        $request = $this->getRequest();
+        if ($request->isPost()){
+            $form->bind($registerNote);
+            $form->setData($request->getPost());
+            if ($form->isValid()){
+                print_r($form->getData());
+            }
+        }
+
+        return array('form' => $form);
     }
 
 
