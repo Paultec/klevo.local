26e1962394f2a255b66c87b2f9815adaecf219d0
Paul Ivan'ko <ipaul.v@gmail.com>
2014-05-09 12:24:14 +0300

    register begin

--- module/Register/src/Register/Controller/RegisterController.php
+++ module/Register/src/Register/Controller/RegisterController.php
@@ -90,7 +90,7 @@ class RegisterController extends AbstractActionController
     }
 
     /**
-     * @return ViewModel
+     * @return
      */
     public function addAction()
     {
@@ -109,7 +109,6 @@ class RegisterController extends AbstractActionController
             if ($form->isValid()){
 
                 $formData = $form->getData();
-                //var_dump($formData);
 
                 $date = new \DateTime($formData['date']);
                 $formData['date'] = $date;
@@ -128,18 +127,14 @@ class RegisterController extends AbstractActionController
                     find(self::USER_ENTITY, $currentUser);
 
                 $registerNote->populate($formData);
-                //var_dump($registerNote);
 
                 $this->getEntityManager()->persist($registerNote);
                 $this->getEntityManager()->flush();
 
                 $idRegister = $registerNote->getId();
-                //var_dump($idRegister);
 
-                $this->forward()->dispatch('Register/Controller/RegisterTable',
+                return $this->forward()->dispatch('Register/Controller/RegisterTable',
                     array('action' => 'add', 'content' => $idRegister));
-                //return $this->redirect()->toRoute('register-table');
-
             }
         }
 
--- module/Register/src/Register/Controller/RegisterTableController.php
+++ module/Register/src/Register/Controller/RegisterTableController.php
@@ -5,6 +5,7 @@ namespace Register\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
+
 class RegisterTableController extends AbstractActionController
 {
 
@@ -16,8 +17,19 @@ class RegisterTableController extends AbstractActionController
     public function addAction()
     {
         $idRegister = $this->params('content');
-        var_dump($idRegister);
-        return new ViewModel(array('idRegister' => $idRegister));
+//        вот так контроллер вызывается:
+        $product = $this->forward()->dispatch('Catalog\Controller\Index', array('action' => 'index'));
+//        а так Ошибка 404 "Запрашиваемый контроллер не смог отправить запрос."
+//        $product = $this->forward()->dispatch('Catalog\Controller\Index');
+//        и так Ошибка 404 "Запрашиваемый контроллер не смог отправить запрос."
+//        $product = $this->forward()->dispatch('Product\Controller\Index', array('action' => 'index'));
+//        и так тоже
+//        $product = $this->forward()->dispatch('Product\Controller\Index');
+//        а так вызывается
+//        $product = $this->forward()->dispatch('Product\Controller\Upload', array('action' => 'index'));
+//        а так опять 404
+//        $product = $this->forward()->dispatch('Product\Controller\Upload');
+        return new ViewModel(array('idRegister' => $idRegister, 'product' => $product));
     }
 
 
