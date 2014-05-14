50c5e6a4728c1625151cf3662920bcf05b21ee64
wizard2014 <wizard2014@ya.ru>
2014-05-14 19:35:54 +0300

    new breadcrumbs

--- module/Product/src/Product/Controller/IndexController.php
+++ module/Product/src/Product/Controller/IndexController.php
@@ -36,22 +36,28 @@ class IndexController extends AbstractActionController
                 ' AND p.idCatalog = ' . $param['catalog']
             );
 
-            $brand    = $this->getEntityManager()->find(self::BRAND_ENTITY, $param['brand']);
-            $category = $this->getEntityManager()->find(self::CATEGORY_ENTITY, $param['catalog']);
+//            $brand    = $this->getEntityManager()->find(self::BRAND_ENTITY, $param['brand']);
+//            $category = $this->getEntityManager()->find(self::CATEGORY_ENTITY, $param['catalog']);
+
+            $brand    = $this->getBreadcrumbs($param, 'brand');
+            $category = $this->getBreadcrumbs($param, 'catalog');
         } elseif (isset($param['brand'])) {
             $dql = $this->getEntityManager()->createQuery(
                 'SELECT p FROM Product\Entity\Product p
                 WHERE p.idBrand = ' . $param['brand']
             );
 
-            $brand    = $this->getEntityManager()->find(self::BRAND_ENTITY, $param['brand']);
+//            $brand    = $this->getEntityManager()->find(self::BRAND_ENTITY, $param['brand']);
+            $brand = $this->getBreadcrumbs($param, 'brand');
         } elseif (isset($param['catalog'])) {
             $dql = $this->getEntityManager()->createQuery(
                 'SELECT p FROM Product\Entity\Product p
                 WHERE p.idCatalog = ' . $param['catalog']
             );
 
-            $category = $this->getEntityManager()->find(self::CATEGORY_ENTITY, $param['catalog']);
+//            $category = $this->getEntityManager()->find(self::CATEGORY_ENTITY, $param['catalog']);
+
+            $category = $this->getBreadcrumbs($param, 'catalog');
         } else {
             $dql = $this->getEntityManager()->createQuery(
                 'SELECT p FROM Product\Entity\Product p'
@@ -79,6 +85,47 @@ class IndexController extends AbstractActionController
         $res->addChild($catalog, 'catalog');
 
         return $res;
+    }
+
+    /**
+     * @param $param
+     * @param $type
+     *
+     * @return array
+     */
+    protected function getBreadcrumbs($param, $type)
+    {
+        if ($type == 'brand') {
+            $brand = $this->getEntityManager()->find(self::BRAND_ENTITY, $param['brand']);
+
+            $arr = array(
+                'Производитель :: ',
+                $brand
+            );
+        } else {
+            $first  = null;
+            $parent = null;
+
+            $catalog = $this->getEntityManager()->find(self::CATEGORY_ENTITY, $param['catalog']);
+
+            if (!is_null($catalog->getIdParent()->getId())) {
+                $parent = $this->getEntityManager()
+                    ->getRepository(self::CATEGORY_ENTITY)->findBy(array('id' => $catalog->getIdParent()->getId()));
+
+                if (!is_null($parent[0]->getIdParent())) {
+                    $first = $this->getEntityManager()
+                        ->getRepository(self::CATEGORY_ENTITY)->findBy(array('id' => $parent[0]->getIdParent()->getId()));
+                }
+            }
+
+            $arr = array(
+                $first,
+                $parent,
+                $catalog
+            );
+        }
+
+        return $arr;
     }
 
     /**
