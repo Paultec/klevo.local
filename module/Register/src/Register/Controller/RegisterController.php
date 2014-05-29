6e60ce9274c80cca911d753aea0a86dc8cfaffab
Paul Ivan'ko <ipaul.v@gmail.com>
2014-05-29 10:53:42 +0300

    register table begin

--- module/Register/src/Register/Controller/RegisterController.php
+++ module/Register/src/Register/Controller/RegisterController.php
@@ -94,33 +94,153 @@ class RegisterController extends AbstractActionController
     public function indexAction()
     {
         $filter = array();
+        $data = array();
+
         $request = $this->getRequest();
+
+        $currentSession = new Container();
+
         if ($request->getPost()) {
-            $filter['beginDate'] = $request->getPost('beginDate');
+            if ($request->getPost('beginDateReset')) {
+                unset($currentSession->beginDate);
+            }
+            if ($request->getPost('beginDate')) {
+                $filter['beginDate'] = $request->getPost('beginDate');
+                $currentSession->beginDate = $filter['beginDate'];
+            } elseif ($currentSession->beginDate) {
+                $filter['beginDate'] = $currentSession->beginDate;
+            }
+
+            if ($request->getPost('endDateReset')) {
+                unset($currentSession->endDate);
+            }
+            if ($request->getPost('endDate')) {
+                $filter['endDate'] = $request->getPost('endDate');
+                $currentSession->endDate = $filter['endDate'];
+            } elseif ($currentSession->endDate) {
+                $filter['endDate'] = $currentSession->endDate;
+            }
+
+            if ($request->getPost('storeFromReset')) {
+                unset($currentSession->storeFrom);
+                unset($currentSession->idStoreFrom);
+            }
+            if ($request->getPost('storeFrom')) {
+                list($filter['storeFrom'], $idStoreFrom) = explode('#', $request->getPost('storeFrom'));
+                $currentSession->storeFrom = $filter['storeFrom'];
+                $currentSession->idStoreFrom = $idStoreFrom;
+            } elseif ($currentSession->storeFrom) {
+                $filter['storeFrom'] = $currentSession->storeFrom;
+                $idStoreFrom = $currentSession->idStoreFrom;
+            }
+
+            if ($request->getPost('storeToReset')) {
+                unset($currentSession->storeTo);
+                unset($currentSession->idStoreTo);
+            }
+            if ($request->getPost('storeTo')) {
+                $filter['storeTo'] = $request->getPost('storeTo');
+                $currentSession->storeTo = $filter['storeTo'];
+                $filter['idStoreTo'] = $request->getPost('idStoreTo');
+                $currentSession->idStoreTo = $filter['idStoreTo'];
+            } elseif ($currentSession->storeTo) {
+                $filter['storeTo'] = $currentSession->storeTo;
+                $filter['idStoreTo'] = $currentSession->idStoreTo;
+            }
+
+            if ($request->getPost('operationReset')) {
+                unset($currentSession->operation);
+            }
+            if ($request->getPost('operation')) {
+                $filter['operation'] = $request->getPost('operation');
+                $currentSession->operation = $filter['operation'];
+            } elseif ($currentSession->operation) {
+                $filter['operation'] = $currentSession->operation;
+            }
+
+            if ($request->getPost('paymentTypeReset')) {
+                unset($currentSession->paymentType);
+            }
+            if ($request->getPost('paymentType')) {
+                $filter['paymentType'] = $request->getPost('paymentType');
+                $currentSession->paymentType = $filter['paymentType'];
+            } elseif ($currentSession->paymentType) {
+                $filter['paymentType'] = $currentSession->paymentType;
+            }
+
+            if ($request->getPost('statusReset')) {
+                unset($currentSession->status);
+            }
+            if ($request->getPost('status')) {
+                $filter['status'] = $request->getPost('status');
+                $currentSession->status = $filter['status'];
+            } elseif ($currentSession->status) {
+                $filter['status'] = $currentSession->status;
+            }
+
+            if ($request->getPost('userReset')) {
+                unset($currentSession->user);
+            }
+            if ($request->getPost('user')) {
+                $filter['user'] = $request->getPost('user');
+                $currentSession->user = $filter['user'];
+            } elseif ($currentSession->user) {
+                $filter['user'] = $currentSession->user;
+            }
         }
 
-        if (null) {
-            $dql = null;
-        } else {
-            $dql = $this->getEntityManager()->createQuery('SELECT r FROM Register\Entity\Register r');
+        $storeList = $this->getEntityManager()->getRepository(self::STORE_ENTITY)->findAll();
+        $data['storeFrom'] = $storeList;
+        $data['storeTo'] = $storeList;
+
+        $operationList = $this->getEntityManager()->getRepository(self::OPERATION_ENTITY)->findAll();
+        $data['operation'] = $operationList;
+
+        $paymentTypeList = $this->getEntityManager()->getRepository(self::PAYMENT_TYPE_ENTITY)->findAll();
+        $data['paymentType'] = $paymentTypeList;
+
+        $statusList = $this->getEntityManager()->getRepository(self::STATUS_ENTITY)->findAll();
+        $data['status'] = $statusList;
+
+        $userList = $this->getEntityManager()->getRepository(self::USER_ENTITY)->findAll();
+        $data['user'] = $userList;
+
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->add('select', 'r')
+            ->add('from', 'Register\Entity\Register r');
+
+        if (isset($filter['beginDate'])) {
+            $qb->andWhere('r.date >= :beginDate')
+               ->setParameter('beginDate', $filter['beginDate']);
         }
+
+        if (isset($filter['endDate'])) {
+            $qb->andWhere('r.date <= :endDate')
+               ->setParameter('endDate', $filter['endDate']);
+        }
+
+//        if (isset($filter['storeFrom'])) {
+//            $qb->andWhere('r.idStoreFrom = :idStoreFrom')
+//                ->setParameter('idStoreFrom', $request->getPost('idStoreFrom'));
+//        }
 
         // Pagination
         $matches = $this->getEvent()->getRouteMatch();
         $page    = $matches->getParam('page', 1);
 
-        $adapter   = new DoctrineAdapter(new ORMPaginator($dql));
+        $adapter   = new DoctrineAdapter(new ORMPaginator($qb));
         $paginator = new Paginator($adapter);
 
         $paginator
             ->setCurrentPageNumber($page)
-            ->setItemCountPerPage(12);
+            ->setItemCountPerPage(20);
 
         //$register = $this->getEntityManager()->getRepository(self::REGISTER_ENTITY)->findAll();
 
         return new ViewModel(array(
             'paginator' => $paginator,
             'filter'    => $filter,
+            'data'      => $data,
         ));
     }
 
